1c3796b39ee32f5b08f9c7176eecb926a3fac773
nuuuwan <nuuuwan@gmail.com>
2021-07-29 17:23:50 +0530

    [add-tweet-file] Done (2021-07-29-1723)

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ pip install tweeting-nuuuwan
 
 ### 1.0.0 (PRE-RELEASE)
 * [2021-07-29 04:16PM] Update Initial Files
+* [2021-07-29 04:31PM] Add tweet file
--- src/tweeting/tweet.py
+++ src/tweeting/tweet.py
@@ -1,10 +1,24 @@
 import argparse
+import sys
 
+from utils import filex
 from utils.twitter import Twitter
 
 
 def _stub():
     return True
+
+
+def split_text(text):
+    MAX_SPLIT_CHARS = 240
+    sentences = text.split('.')
+    split_text_list = ['']
+    for sentence in sentences:
+        if len(split_text_list[-1]) + len(sentence) > MAX_SPLIT_CHARS + 2:
+            split_text_list.append('')
+        split_text_list[-1] += sentence + '.'
+
+    return split_text_list
 
 
 def _run():
@@ -26,10 +40,13 @@ def _run():
     args = parser.parse_args()
 
     tweet_text = args.tweet_text
-    tweet_text_file = args.tweet_text
+    tweet_text_file = args.tweet_text_file
     if not (tweet_text_file or tweet_text):
         print('One of tweet_text or tweet_text_file is required!')
-        return False
+        return sys.exit(-1)
+
+    if not tweet_text:
+        tweet_text = filex.read(tweet_text_file)
 
     twtr = Twitter(
         args.twtr_api_key,
@@ -37,10 +54,14 @@ def _run():
         args.twtr_access_token,
         args.twtr_access_token_secret,
     )
-    if not twtr:
-        return False
-
-    twtr.tweet(tweet_text)
+    prev_id = None
+    for tweet_text_single in split_text(tweet_text):
+        if not prev_id:
+            prev_id = twtr.tweet(tweet_text_single).id
+        else:
+            prev_id = twtr.tweet(
+                tweet_text_single, in_reply_to_status_id=prev_id
+            ).id
 
 
 if __name__ == '__main__':
